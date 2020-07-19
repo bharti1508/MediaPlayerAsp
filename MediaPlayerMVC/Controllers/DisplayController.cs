025249029c0c8329afd1e68bc2820c15f43125ba
BhartiGarde <bhartigarde@gmail.com>
2020-07-19 20:20:15 +0530

    Added registration and login pages with firebase database

---
+++ MediaPlayerMVC/Controllers/DisplayController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using MediaPlayerMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace MediaPlayerMVC.Controllers
+{
+    public class DisplayController : Controller
+    {
+
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "njOwVqAgVmgIrxLO8HrDK92CZFiLetuEyuVReDiU",
+            BasePath = "https://mediaplayerasp.firebaseio.com/"
+        };
+
+        IFirebaseClient client;
+
+        public ActionResult Index()
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Users");
+           
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+           
+            var list = new List<RegistrationModel>();
+            foreach (var item in data)
+            {
+                Console.WriteLine(JsonConvert.DeserializeObject<RegistrationModel>(((JProperty)item).Value.ToString()).Name);
+                list.Add(JsonConvert.DeserializeObject<RegistrationModel>(((JProperty)item).Value.ToString()));
+
+            }
+            
+            return View(list);
+        }
+
+       
+    }
+}
---
+++ MediaPlayerMVC/Models/RegistrationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaPlayerMVC.Models
+{
+    public class RegistrationModel
+    {
+        public string Key { get; set; }
+
+        [Required]
+        [Display(Name = "Enter Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Enter Username")]
+        public string Username { get; set; }
+
+        [Required]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Enter Password")]
+        public string Password { get; set; }
+
+    }
+}
